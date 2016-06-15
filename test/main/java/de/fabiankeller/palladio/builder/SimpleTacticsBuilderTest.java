65788bdb3770e0e173f037ec1b64c8a6db1ecfd6
Fabian Keller <hello@fabian-keller.de>
2016-06-15 17:14:19 +0200

    Update: simple tactics example with resource environment

--- test/main/java/de/fabiankeller/palladio/builder/SimpleTacticsBuilderTest.java
+++ test/main/java/de/fabiankeller/palladio/builder/SimpleTacticsBuilderTest.java
@@ -6,6 +6,10 @@ import de.fabiankeller.palladio.builder.repository.InterfaceBuilder;
 import de.fabiankeller.palladio.builder.repository.ParameterType;
 import de.fabiankeller.palladio.builder.repository.RepositoryBuilder;
 import de.fabiankeller.palladio.builder.repository.impl.RepositoryBuilderImpl;
+import de.fabiankeller.palladio.builder.resourceenvironment.ContainerBuilder;
+import de.fabiankeller.palladio.builder.resourceenvironment.LinkBuilder;
+import de.fabiankeller.palladio.builder.resourceenvironment.ResourceEnvironmentBuilder;
+import de.fabiankeller.palladio.builder.resourceenvironment.impl.ResourceEnvironmentBuilderImpl;
 import de.fabiankeller.palladio.builder.system.AssemblyBuilder;
 import de.fabiankeller.palladio.builder.system.SystemBuilder;
 import de.fabiankeller.palladio.builder.system.impl.SystemBuilderImpl;
@@ -15,6 +19,8 @@ public class SimpleTacticsBuilderTest {
 
     private final RepositoryBuilder repo = new RepositoryBuilderImpl();
     private final SystemBuilder system = new SystemBuilderImpl();
+    // fixme: DI instead of null
+    private final ResourceEnvironmentBuilder env = new ResourceEnvironmentBuilderImpl(null);
 
     public void testSimpleTactics() {
 
@@ -66,5 +72,34 @@ public class SimpleTacticsBuilderTest {
         a_bookingSystem.provideFor(a_businessTripMgmt, i_booking);
         a_paymentSystem.provideFor(a_businessTripMgmt, i_employeePayment);
         a_bookingSystem.provideFor(a_paymentSystem, i_externalPayment);
+
+
+        // // // RESOURCE ENVIRONMENT // // //
+        final ContainerBuilder e_server1 = this.env.createContainer("Server1")
+                .withCpu(3.5)
+                .withMTTF(300000.0)
+                .withMTTR(6.0)
+                .withNumberOfReplicas(1)
+                .withRequiredByContainer(false)
+                .end();
+        final ContainerBuilder e_server2 = this.env.createContainer("Server2")
+                .withCpu(4.0)
+                .withMTTF(250000.0)
+                .withMTTR(3.0)
+                .withNumberOfReplicas(1)
+                .withRequiredByContainer(false)
+                .end();
+        final ContainerBuilder e_server3 = this.env.createContainer("Server3")
+                .withCpu(3.0)
+                .withMTTF(275000.0)
+                .withMTTR(4.0)
+                .withNumberOfReplicas(1)
+                .withRequiredByContainer(false)
+                .end();
+        final LinkBuilder e_network = this.env.createLink("Network")
+                .between(e_server1, e_server2, e_server3)
+                .withLatency(0.015)
+                .withThroughput(2000000000)
+                .withFailureProbability(2.0e-4);
     }
 }
