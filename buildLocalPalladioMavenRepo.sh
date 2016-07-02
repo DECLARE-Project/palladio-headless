3754a9effbd289a92a8d63d855b0b46fcd386783
Fabian Keller <hello@fabian-keller.de>
2016-07-02 13:14:56 +0200

    Update: only import actual dependencies into local maven repository

--- buildLocalPalladioMavenRepo.sh
+++ buildLocalPalladioMavenRepo.sh
@@ -18,15 +18,29 @@ echo "  Palladio Directory OK"
 # build maven repo
 mkdir -p ./pathing/.m2/
 
-for plugin in $(find $1/plugins -maxdepth 1 -type f -name '*.jar' ); do
-    name=$(echo $plugin | sed -rn 's/^.+\/([^_]+)\_.*?$/\1/p')
+#for name in $(cat pom.xml | sed -rn "s/^\s*<artifactId>([^<]+).+$/\1/p"); do
+for name in $(cat pom.xml | sed -rn '
+    /^.*de\.fabiankeller\.palladio<\/groupId>\s*$/{    # if line matches group
+        N                                              # read next line into pattern space
+        s/.*<artifactId>([^<]+).*/\1/p                 # print artifactId
+    }'); do
+
+    # try to find plugin jar file with the given name
+    plugin=$(find $1/plugins -maxdepth 1 -type f -name "$name\_*.jar" | tail -n 1)
+
+    if [ -z "$plugin" ]; then
+        echo "  WARNING >>> $name could not be found in any version in $1/plugins. Please install the appropriate Palladio extension containing the JAR."
+        continue
+    fi
     version=$(echo $plugin | sed -rn 's/^[^_]+\_(.*?)\.jar$/\1/p')
 
+    # check if it is already imported
     if [ -f ./pathing/.m2/de/fabiankeller/palladio/$name/$version/$name-$version.pom ]; then
         echo "  Skipping >>> $name :: $version, as pom is already present"
         continue
     fi
 
+    # import as maven dependency
     echo "  Adding >>> $name :: $version"
     mvn install:install-file \
         -Dfile=$plugin \
