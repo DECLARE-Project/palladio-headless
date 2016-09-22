402515fb419a74242da3ddc134a7c95299d63cc8
Fabian Keller <hello@fabian-keller.de>
2016-09-22 15:02:51 +0200

    Feature(analysis): define result value objects

---
+++ palladio-analysis/src/main/java/de/fabiankeller/palladio/analysis/result/valueobject/Percentage.java
@@ -0,0 +1,46 @@
+package de.fabiankeller.palladio.analysis.result.valueobject;
+
+public class Percentage implements ValueObject {
+
+    private final double percentage;
+
+    protected Percentage(final double percentage1) {
+        this.percentage = percentage1;
+    }
+
+    /**
+     * Creates a percentage.
+     *
+     * @param percentage pass a number in the interval between [0.0;1.0] to represent 0% to 100%.
+     */
+
+    public static Percentage of(final double percentage) {
+        return new Percentage(percentage);
+    }
+
+    public double getPercentage() {
+        return this.percentage;
+    }
+
+    public String toHumanReadable() {
+        return String.format("%.4f%%", 100.0 * getPercentage());
+    }
+
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Percentage that = (Percentage) o;
+
+        return Double.compare(that.percentage, this.percentage) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        final long temp = Double.doubleToLongBits(this.percentage);
+        return (int) (temp ^ (temp >>> 32));
+    }
+}
---
+++ palladio-analysis/src/main/java/de/fabiankeller/palladio/analysis/result/valueobject/ValueObject.java
@@ -0,0 +1,12 @@
+package de.fabiankeller.palladio.analysis.result.valueobject;
+
+/**
+ * Defines value objects for the results.
+ */
+public interface ValueObject {
+
+    /**
+     * Turns the value object into a human readable string.
+     */
+    String toHumanReadable();
+}
