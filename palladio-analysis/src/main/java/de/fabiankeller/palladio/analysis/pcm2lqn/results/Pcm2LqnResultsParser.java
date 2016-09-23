8d418105592dd9e7a2eacd4ce3b1e6fbc7ab7a7f
Fabian Keller <hello@fabian-keller.de>
2016-09-23 11:36:22 +0200

    Feature(analysis): basic implementation of Pcm2LqnResultsParser

--- palladio-analysis/src/main/java/de/fabiankeller/palladio/analysis/pcm2lqn/results/Pcm2LqnResultsParser.java
+++ palladio-analysis/src/main/java/de/fabiankeller/palladio/analysis/pcm2lqn/results/Pcm2LqnResultsParser.java
@@ -2,10 +2,19 @@ package de.fabiankeller.palladio.analysis.pcm2lqn.results;
 
 import de.fabiankeller.palladio.analysis.result.PerformanceResultWriter;
 import de.fabiankeller.palladio.analysis.result.exception.InvalidResultException;
+import de.fabiankeller.palladio.analysis.result.type.ServiceTime;
+import de.fabiankeller.palladio.analysis.result.type.Utilization;
+import de.fabiankeller.palladio.analysis.result.valueobject.Duration;
+import de.fabiankeller.palladio.analysis.result.valueobject.Percentage;
 import de.fabiankeller.palladio.analysis.tracing.PcmModelTrace;
 import org.palladiosimulator.pcm.core.entity.NamedElement;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
+import org.palladiosimulator.pcm.seff.AbstractAction;
 import org.palladiosimulator.solver.lqn.*;
 import org.palladiosimulator.solver.transformations.pcm2lqn.LqnXmlHandler;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 /**
  * Parses the output of the Palladio PCM2LQN Analysis tool and maps it to the PCM instance objects.
@@ -49,6 +58,15 @@ public class Pcm2LqnResultsParser {
     }
 
     private void extractProcessor(final ProcessorType proc) {
+        // in case the processor is a {@link ResourceContainer}, we'll attach the utilization result
+        if (proc.getResultProcessor().size() == 1) {
+            this.<ResourceContainer>traceElement(proc.getName()).ifPresent(resourceContainer -> {
+                final OutputResultType result = proc.getResultProcessor().get(0);
+                this.rw.attach(new Utilization<>(resourceContainer, Percentage.of(result.getUtilization())));
+            });
+        }
+
+        // extract tasks
         proc.getTask().forEach(this::extractTask);
     }
 
@@ -57,8 +75,67 @@ public class Pcm2LqnResultsParser {
     }
 
     private void extractEntry(final EntryType entry) {
+        final PhaseActivities phaseActivities = entry.getEntryPhaseActivities();
+        if (null != phaseActivities) {
+            phaseActivities.getActivity().forEach(this::extractActivity);
+        }
+    }
 
+    private void extractActivity(final ActivityPhasesType activity) {
+        if (activity.getResultActivity().size() == 1) {
+            this.extractActivityResult(activity, activity.getResultActivity().get(0));
+        }
+    }
+
+    private void extractActivityResult(final ActivityPhasesType activity, final OutputResultType result) {
+        assert null != result;
+
+        // get the action the result applies to
+        final Optional<AbstractAction> optionalAction = this.<AbstractAction>traceElement(activity.getName());
+        if (!optionalAction.isPresent()) {
+            return;
+        }
+        final AbstractAction action = optionalAction.get();
+
+        // extract utilization
+        if (result.isSetUtilization()) {
+            this.rw.attach(new Utilization<AbstractAction>(action, Percentage.of(result.getUtilization())));
+        }
+
+        // extract service time
+        if (result.isSetServiceTime()) {
+            this.rw.attach(new ServiceTime<AbstractAction>(action, Duration.ofMilliseconds(result.getServiceTime())));
+        }
+
+        /*
+            TODO: extract the following result types:
+            - proc-waiting
+            - service-time-variance
+            - throughput
+            - throughput-bound
+            - proc-utilization
+            - proc-waiting
+         */
     }
 
 
+    /**
+     * Calls {@link PcmModelTrace::findByString} with the given name and tries to cast the found element to the given
+     * class.
+     * <p>
+     * Note: If the cast is not successful, an empty optional will be returned.
+     *
+     * @throws NoSuchElementException in case the trace does not contain an element identified by the given name
+     */
+    private <T extends NamedElement> Optional<T> traceElement(final String name) {
+        final NamedElement el = this.trace
+                .findByString(name)
+                .orElseThrow(() -> new NoSuchElementException(String.format("Could not find '%s' in trace.", name)));
+        try {
+            return Optional.of((T) el);
+        } catch (final ClassCastException e) {
+            // found element is not of correct type
+            return Optional.empty();
+        }
+    }
 }
