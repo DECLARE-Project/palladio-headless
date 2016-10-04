59f0463eb45ad709d1a0c269201ee6ef6d54453a
Fabian Keller <hello@fabian-keller.de>
2016-10-04 13:40:25 +0200

    Update(performance): remove palladio reference from docs

--- performance-analysis/src/main/java/de/fabiankeller/performance/analysis/result/Result.java
+++ performance-analysis/src/main/java/de/fabiankeller/performance/analysis/result/Result.java
@@ -3,7 +3,7 @@ package de.fabiankeller.performance.analysis.result;
 import de.fabiankeller.performance.analysis.result.metric.PerformanceMetric;
 
 /**
- * Represents a single performance analysis result, that is specific for a element in the PCM.
+ * Represents a single performance analysis result, that is specific for a certain model element.
  *
  * @param <T> constrain the types that a result can be attached to
  */
