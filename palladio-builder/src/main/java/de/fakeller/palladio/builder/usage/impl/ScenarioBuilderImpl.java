367e66772103522a5c0a6bb43671b62d011b56ad
Fabian Keller <hello@fabian-keller.de>
2016-10-29 22:24:57 +0200

    Feature(builder): support closed workload

--- palladio-builder/src/main/java/de/fakeller/palladio/builder/usage/impl/ScenarioBuilderImpl.java
+++ palladio-builder/src/main/java/de/fakeller/palladio/builder/usage/impl/ScenarioBuilderImpl.java
@@ -8,6 +8,7 @@ import de.fakeller.palladio.builder.usage.ScenarioBuilder;
 import de.fakeller.palladio.builder.usage.UsageBuilder;
 import de.fakeller.palladio.builder.util.random.RandomVariable;
 import de.fakeller.palladio.builder.util.random.RandomVariableFactory;
+import org.palladiosimulator.pcm.usagemodel.ClosedWorkload;
 import org.palladiosimulator.pcm.usagemodel.OpenWorkload;
 import org.palladiosimulator.pcm.usagemodel.UsageScenario;
 import org.palladiosimulator.pcm.usagemodel.UsagemodelFactory;
@@ -47,6 +48,20 @@ public class ScenarioBuilderImpl extends AbstractHierarchicalBuilder<ScenarioBui
         // create model
         final OpenWorkload wl = UsagemodelFactory.eINSTANCE.createOpenWorkload();
         wl.setInterArrivalTime_OpenWorkload(RandomVariableFactory.factory().exp(interArrivalTime).get());
+
+        // link model
+        this.eModel.setWorkload_UsageScenario(wl);
+        wl.setUsageScenario_Workload(this.eModel);
+
+        return this;
+    }
+
+    @Override
+    public ScenarioBuilder withClosedWorkload(final int population, final RandomVariable thinkTime) {
+        // create model
+        final ClosedWorkload wl = UsagemodelFactory.eINSTANCE.createClosedWorkload();
+        wl.setPopulation(population);
+        wl.setThinkTime_ClosedWorkload(thinkTime.get());
 
         // link model
         this.eModel.setWorkload_UsageScenario(wl);
